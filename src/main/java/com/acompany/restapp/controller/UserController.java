d0fb1e637c7646d38b5a417465a6d40483c76feb
soongon <soongon@hucloud.co.kr>
2019-06-02 13:16:12 +0900

    6, 7차시 완료

---
+++ src/main/java/com/acompany/restapp/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.acompany.restapp.controller;
+
+import com.acompany.restapp.model.User;
+import com.acompany.restapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("")
+    public List<User> getAllUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userid}")
+    public User getUserByUserid(@PathVariable Integer userid) {
+        System.out.println(userid);
+        return userService.getUserById(userid);
+    }
+
+    @PostMapping("")
+    public User registUser(@RequestBody User user) {
+        System.out.println(user);
+        return userService.registUser(user);
+    }
+
+    @PutMapping("/{userid}")
+    public void modifyUser(
+            @PathVariable Integer userid,
+            @RequestBody User user) {
+        userService.modifyUser(userid, user);
+    }
+
+    @DeleteMapping("/{userid}")
+    public void removeUser(@PathVariable Integer userid) {
+        userService.removeUser(userid);
+    }
+}
--- src/main/java/com/acompany/restapp/service/UserService.java
+++ src/main/java/com/acompany/restapp/service/UserService.java
@@ -1,11 +1,11 @@
 package com.acompany.restapp.service;
 
 import com.acompany.restapp.dao.UserRepository;
+import com.acompany.restapp.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -16,5 +16,26 @@ public class UserService {
 
     public Map<String, String> getMessage() {
         return userRepository.getMessage();
+    }
+
+    public List<User> getUsers() {
+        return userRepository.getAllUsers();
+    }
+
+    public User getUserById(Integer userid) {
+        return userRepository.getUserByUserid(userid);
+    }
+
+    public User registUser(User user) {
+        return userRepository.insertUser(user);
+    }
+
+    public void modifyUser(Integer userid, User user) {
+        user.setUserid(userid);
+        userRepository.updateUser(user);
+    }
+
+    public void removeUser(Integer userid) {
+        userRepository.deleteUser(userid);
     }
 }
